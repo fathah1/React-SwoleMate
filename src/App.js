f7f3a0e225c482dd1b4cf177605b723cbf9f3661
fathah1 <fathah1@gmail.com>
2023-07-23 20:00:42 +0400

    structuring and routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from 'react'
 import './App.css'
+import {Route, Routes} from 'react-router-dom'
+import { Box } from '@mui/material'
+import Home from './pages/Home';
+import Navbar from './components/Navbar';
+import ExerciseDetail from './pages/ExerciseDetail';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
-    <div>App</div>
+    <Box width="400px" sx={{width: {xl: '1488px'}}} m="auto">
+        <Navbar/>
+        <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/excercise/:id" element={<ExerciseDetail/>} />
+        </Routes>
+        <Footer/>
+    </Box>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import ReactDOM  from 'react-dom';
 import React from 'react';
 import App from './App';
+import {BrowserRouter} from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(<App/>)
+root.render(
+<BrowserRouter>
+<App/>
+</BrowserRouter>
+
+)
