70b5912310360ea4ca23e6146f2d914d4d4449b1
fathah1 <fathah1@gmail.com>
2023-07-31 20:51:18 +0400

    search Excercise basic

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -5,7 +5,7 @@ import HeroBannerImage from '../assets/images/banner2.png'
 const HeroBanner = () => {
   return (
     <Box sx={{
-        mt:{lg:'212px',xs:'70px'},ml:{sm:'50px'}
+        mt:{lg:'170px',xs:'70px'},ml:{sm:'50px'}
     }} position="relative" p="20px">
         <Typography color="#FF2625" fotWeight="600" fontSize="26px">Swole Mate</Typography>
         <Typography fontWeight={700} sx={{fontSize: {lg:'44px',xs:'40px'}}} mb="23px" mt="30px">
@@ -20,7 +20,7 @@ const HeroBanner = () => {
         <Typography fontWeight={600} color="#ff2625" sx={{opacity:0.1,display:{lg:'block',xs:'none'}}} fontSize="200px">
             Exercise
         </Typography>
-        <img src={HeroBannerImage} alt="image" className="hero-banner-img" />
+        <img src={HeroBannerImage} alt="image" className="hero-banner-img" sx={{}} />
     </Box>
   )
 }
--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import {Box, Button, Stack, Typography, TextField } from '@mui/material'
+import React, {useEffect, useState} from 'react'
 
 const SearchExercises = () => {
   return (
-<div>SearchExercises</div>
-  )
+<Stack alignItems="center" mt="37px" justifyContent="center" p="20px">
+  <Typography fontWeight={700} sx={{fontSize:{lg:'44px', xs:'30px',}}} mb="50px" textAlign="center">
+    Some Excercises To <br/> Get Swole
+  </Typography>
+
+  <Box position="relative" mb="72px">
+    <TextField height="76px" value="" onChange={(e) => {}} placeHolder="Search Excercise" type="text" sx={{input: {fontWeight:'700', border:'none', borderRadius:'4px' }, width: {lg:'800px', xs:'350px'}
+  ,backgroundColor:'#fff', borderRadius:'40px'}}/>
+  <Button className="search-btn" sx={{bgcolor:'#FF2625',color:'#fff', textTransform:'none' ,width:{lg:'175px',xs:'80px'},height:'56px',position:"absolute",right:'0'}}>Search</Button>
+  </Box>
+
+</Stack>
+)
 }
 
 export default SearchExercises
